de268b3dc5098935455068f1ed62d82f3a0199ac
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2025-05-23 12:01:55 +0530

    Handling requests in streamlit complete

---
+++ 05_handling_api_requests.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import requests
+
+st.title("Live currency converter")
+
+inr = st.number_input("Enter the amount in INR", step=0.1)
+
+target_currency = st.selectbox(
+    "Choose the currency to convert to:", ["USD", "EUR", "JPY"]
+)
+
+if st.button("Convert"):
+    url = "https://api.exchangerate-api.com/v4/latest/INR"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        rate = data.get("rates", {}).get(target_currency, 0)
+        if rate != 0:
+            st.success(f"{inr} INR = {rate*inr } {target_currency}")
+    else:
+        st.error("Failed to fetch currency data.")
